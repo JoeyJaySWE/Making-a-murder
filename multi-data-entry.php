b1054b9c8514380563c3c69064c4841d05e3f94f
amanda.fager@outlook.com <amanda.fager@outlook.com>
2020-10-30 15:31:47 +0100

    added data

--- multi-data-entry.php
+++ multi-data-entry.php
@@ -19,31 +19,32 @@ function db(): PDO
 
     return $pdo;
 }
-$query = "insert into person (person_id, full_name, address_number, address_street_name, gender) values ('61-288-3709', 'Nevile Lockner', '66', 'Longview', 'Male');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('49-871-3081', 'Kris Girardin', '98', 'Buena Vista', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('30-571-2876', 'Nowell Conibeer', '473', 'Thackeray', 'Male');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('51-039-4118', 'Chaunce Braisted', '56451', 'Talisman', 'Male');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('65-164-3035', 'Rasla Kintzel', '3', 'Sycamore', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('34-580-0449', 'Benoite Maryon', '88861', 'Tomscot', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('43-966-2119', 'Heinrick Stallebrass', '397', 'Montana', 'Male');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('84-904-0358', 'Matty Pattingson', '0', 'Valley Edge', 'Male');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('65-969-2419', 'Ulrica Willett', '19', 'Sloan', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('81-365-9615', 'Claiborn Putton', '83', 'Schmedeman', 'Male');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('40-055-5863', 'Etan Faber', '66', 'Packers', 'Male');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('01-308-8387', 'Xever MacNamara', '068', 'Becker', 'Male');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('69-951-2052', 'Winnah Sorrel', '56676', 'Bunker Hill', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('91-620-2768', 'Miller Backhurst', '48330', 'Nobel', 'Male');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('69-979-2765', 'Reggie Wharin', '9828', 'Mosinee', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('97-906-8731', 'Jackie Le Franc', '29', 'Sage', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('10-312-8826', 'Lari Brissenden', '3', 'Trailsway', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('69-127-2856', 'Roda Winnister', '758', 'Katie', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('86-931-3754', 'Orin Breche', '567', 'Lillian', 'Male');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('26-223-9291', 'Danice McMichell', '3', 'School', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('48-794-5113', 'Orlando Shoard', '2', 'Cottonwood', 'Male');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('16-529-8112', 'Lil McGriele', '8', 'Waubesa', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('54-908-5536', 'Merlina Sambrook', '98119', 'Almo', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('30-092-0364', 'Arlen Mattingley', '31', 'Kensington', 'Female');
-insert into person (person_id, full_name, address_number, address_street_name, gender) values ('85-548-7721', 'Silvano Todd', '63', 'Corscot', 'Male');
+$query = "insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('18-738-8654', '3558771083932794', '04', 'Tennessee', 35);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('28-935-7973', '3569670998489394', '911', 'Kingsford', 93);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('17-799-6045', '201861666169448', '3871', 'Saint Paul', 90);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('54-015-5523', '5602249171413754', '3', 'Acker', 85);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('14-439-4768', '3544343070170874', '4267', 'Kim', 73);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('29-574-9719', '3542360779216149', '119', 'Mendota', 60);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('36-491-6048', '3560398242196180', '2125', 'Quincy', 21);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('74-901-6360', '201594434704739', '990', 'Thompson', 64);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('76-564-4353', '6706958238785755', '10', 'Raven', 19);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('81-750-2320', '501828363855286687', '837', 'Waywood', 56);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('20-641-4175', '67711989804031170', '0', 'Rockefeller', 20);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('50-856-8056', '374288307456722', '5411', 'Moland', 28);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('91-074-9815', '201686383241133', '7027', 'Daystar', 59);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('91-448-4553', '4041371414632', '63', 'Meadow Valley', 39);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('52-492-3465', '3584654735318661', '0526', 'Emmet', 53);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('24-553-7485', '201962827377076', '8422', 'Linden', 50);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('27-712-7939', '5317294051735641', '8626', 'Tony', 67);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('82-218-4620', '3584490652625360', '923', 'Sunnyside', 92);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('78-589-1027', '3588422759307036', '329', 'Longview', 51);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('68-674-6478', '374288330289629', '93248', 'Farragut', 74);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('46-155-0589', '3551614226608664', '3', 'East', 64);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('02-820-6831', '5602253348471778', '2360', 'Oxford', 26);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('48-004-3295', '5100142008383893', '33727', 'Wayridge', 82);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('90-623-1244', '3528812695344692', '7', 'Miller', 61);
+insert into bank (person_id, bank_card_number, address_number, address_street_name, card_pin) values ('28-745-2571', '3583485590287910', '4', 'Scoville', 68);
+
 ";
 
 $stmt = db()->exec($query);
