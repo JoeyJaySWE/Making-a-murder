9e3f4565ce1f01b306408de02d9c788f2212a0e9
amanda.fager@outlook.com <amanda.fager@outlook.com>
2020-11-05 16:42:20 +0100

    changes

--- multi-data-entry.php
+++ multi-data-entry.php
@@ -19,31 +19,32 @@ function db(): PDO
 
     return $pdo;
 }
-$query = "insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (55, '1/31/2020', '6331106484979739', 62, '2.199.203.28', 'Goat - Whole Cut');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (79, '5/24/2020', '30416327447571', 54, '214.118.205.195', 'Danishes - Mini Raspberry');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (11, '9/22/2020', '3548128021407392', 22, '244.91.9.230', 'Placemat - Scallop, White');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (85, '1/21/2020', '3587552928837918', 7, '50.122.211.246', 'Bagel - Plain');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (86, '2/21/2020', '374014845869303', 39, '195.112.120.195', 'Beans - Kidney, Red Dry');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (39, '8/26/2020', '670638723605399002', 61, '88.230.243.24', 'Sauce - Gravy, Au Jus, Mix');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (82, '9/16/2020', '6762358133311915306', 30, '106.175.34.135', 'Fish - Halibut, Cold Smoked');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (3, '1/11/2020', '3571142794336423', 21, '5.70.188.189', 'Coffee Cup 16oz Foam');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (12, '5/10/2020', '5436468458240662', 41, '30.134.108.139', 'Juice - Clamato, 341 Ml');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (77, '10/8/2020', '3578498805115666', 96, '133.164.251.171', 'Napkin - Cocktail,beige 2 - Ply');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (95, '2/21/2020', '3538341125472475', 5, '174.142.59.195', 'Wine - Red, Concha Y Toro');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (29, '7/10/2020', '3538436480408450', 6, '206.133.74.63', 'Cinnamon Buns Sticky');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (96, '12/12/2019', '3585229232206054', 62, '234.36.49.86', 'Juice - Ocean Spray Cranberry');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (76, '5/25/2020', '4844523682780906', 53, '100.35.111.163', 'Sesame Seed Black');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (70, '6/5/2020', '4508983189194703', 91, '242.166.39.228', 'Irish Cream - Butterscotch');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (10, '11/11/2019', '3535384571719166', 81, '199.45.100.188', 'Tomatillo');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (15, '6/16/2020', '6767364626864951637', 16, '248.40.115.222', 'Mushroom Morel Fresh');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (23, '2/1/2020', '201489868858335', 73, '223.223.159.237', 'Juice Peach Nectar');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (91, '3/5/2020', '3546166819694468', 97, '194.115.115.51', 'Sausage - Meat');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (46, '12/28/2019', '30463162172658', 40, '29.97.190.194', 'Pike - Frozen Fillet');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (18, '9/29/2020', '3579933288501023', 31, '116.108.160.214', 'Juice - Apple Cider');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (87, '12/8/2019', '201992199240511', 44, '240.8.194.203', 'Pork - Loin, Bone - In');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (43, '9/26/2020', '3574216379878079', 9, '153.201.190.120', 'Shrimp - 100 / 200 Cold Water');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (58, '8/13/2020', '5100141323813311', 57, '117.198.65.4', 'Port - 74 Brights');
-insert into receipt_cafeteria (id, date_of_transaction, bank_card_number, total_cost, sales_id, items) values (64, '5/4/2020', '5108757004953357', 26, '12.46.176.153', 'Wine - Zinfandel California 2002');
+$query = "insert into person (person_id, full_name, address_number, address_street_name, gender) values ('61-288-3709', 'Nevile Lockner', '66', 'Longview', 'Male');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('49-871-3081', 'Kris Girardin', '98', 'Buena Vista', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('30-571-2876', 'Nowell Conibeer', '473', 'Thackeray', 'Male');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('51-039-4118', 'Chaunce Braisted', '56451', 'Talisman', 'Male');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('65-164-3035', 'Rasla Kintzel', '3', 'Sycamore', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('34-580-0449', 'Benoite Maryon', '88861', 'Tomscot', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('43-966-2119', 'Heinrick Stallebrass', '397', 'Montana', 'Male');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('84-904-0358', 'Matty Pattingson', '0', 'Valley Edge', 'Male');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('65-969-2419', 'Ulrica Willett', '19', 'Sloan', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('81-365-9615', 'Claiborn Putton', '83', 'Schmedeman', 'Male');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('40-055-5863', 'Etan Faber', '66', 'Packers', 'Male');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('01-308-8387', 'Xever MacNamara', '068', 'Becker', 'Male');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('69-951-2052', 'Winnah Sorrel', '56676', 'Bunker Hill', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('91-620-2768', 'Miller Backhurst', '48330', 'Nobel', 'Male');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('69-979-2765', 'Reggie Wharin', '9828', 'Mosinee', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('97-906-8731', 'Jackie Le Franc', '29', 'Sage', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('10-312-8826', 'Lari Brissenden', '3', 'Trailsway', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('69-127-2856', 'Roda Winnister', '758', 'Katie', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('86-931-3754', 'Orin Breche', '567', 'Lillian', 'Male');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('26-223-9291', 'Danice McMichell', '3', 'School', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('48-794-5113', 'Orlando Shoard', '2', 'Cottonwood', 'Male');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('16-529-8112', 'Lil McGriele', '8', 'Waubesa', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('54-908-5536', 'Merlina Sambrook', '98119', 'Almo', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('30-092-0364', 'Arlen Mattingley', '31', 'Kensington', 'Female');
+insert into person (person_id, full_name, address_number, address_street_name, gender) values ('85-548-7721', 'Silvano Todd', '63', 'Corscot', 'Male');
+
 ";
 
 $stmt = db()->exec($query);
